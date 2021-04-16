1a946681fcdcf234168ed66224e020b78195dff6
Weijie <52730904+simweijie@users.noreply.github.com>
2021-04-16 18:54:09 +0800

    Add files via upload

--- code/index.py
+++ code/index.py
@@ -43,52 +43,52 @@ def handler(event, context):
 # https://stackabuse.com/how-to-sort-dictionary-by-value-in-python/
     branchList = []
     rows = cur.fetchall()
-    id = 0
-    latt = 0
-    longt = 0
-    # Distance and Queue length calculation
-    distanceDict = dict()
-    for row in rows:
-        print("TEST {0} {1} {2} {3}".format(row[0],row[1],row[2],row[3]))
-        id = row[0]
-        latt2 = radians(row[1])
-        longt2 = radians(row[2])
-        coords_1=(latt1,longt1)
-        coords_2=(latt2,longt2)
-        distance = geopy.distance.distance(coords_1, coords_2).km
-        print ("GEOPY DISTANCE: ", distance)
-        distanceDict[id] = distance
-    sorted_distanceDict = dict()
-    sorted_keys = sorted(distanceDict, key=distanceDict.get)
-    count = 1
-    for w in sorted_keys:            
-        if(row[3] > 5):
-            sorted_distanceDict[w] = count + 1
-        else:
-            sorted_distanceDict[w] = count
-        count += 1
-    bestBranch = min(sorted_distanceDict,key=sorted_distanceDict.get)
+    if len(rows) >0:
+        id = 0
+        latt = 0
+        longt = 0
+        # Distance and Queue length calculation
+        distanceDict = dict()
+        for row in rows:
+            print("TEST {0} {1} {2} {3}".format(row[0],row[1],row[2],row[3]))
+            id = row[0]
+            latt2 = radians(row[1])
+            longt2 = radians(row[2])
+            coords_1=(latt1,longt1)
+            coords_2=(latt2,longt2)
+            distance = geopy.distance.distance(coords_1, coords_2).km
+            print ("GEOPY DISTANCE: ", distance)
+            distanceDict[id] = distance
+        sorted_distanceDict = dict()
+        sorted_keys = sorted(distanceDict, key=distanceDict.get)
+        count = 1
+        for w in sorted_keys:            
+            if(row[3] > 5):
+                sorted_distanceDict[w] = count + 1
+            else:
+                sorted_distanceDict[w] = count
+            count += 1
+        bestBranch = min(sorted_distanceDict,key=sorted_distanceDict.get)
 
-    # Distance and Queue length calculation
-    query = "SELECT b.*,count(b.id) as queue FROM Branch b, Queue q WHERE b.id=q.branchId AND b.id={} AND q.status='Q' GROUP BY b.id;".format(bestBranch)
-    cur.execute(query)
-    connection.commit()
-    rows = cur.fetchall()
-    for row in rows:
-        print("TEST {0} {1} {2} {3} {4} {5}".format(row[0],row[1],row[2],row[3],row[4],row[5]))
-        transactionResponse = {}
-        transactionResponse['id'] = row[0]
-        transactionResponse['name'] = row[1]
-        transactionResponse['district'] = row[2]
-        transactionResponse['addr'] = row[3]
-        transactionResponse['postal'] = row[4]
-        transactionResponse['contactNo'] = row[5]
-        transactionResponse['latt'] = row[6]
-        transactionResponse['longt'] = row[7]
-        transactionResponse['clinicId'] = row[8]
-        transactionResponse['queueLength'] = row[9]
-        branchList.append(transactionResponse)
-
+        # Distance and Queue length calculation
+        query = "SELECT b.*,count(b.id) as queue FROM Branch b, Queue q WHERE b.id=q.branchId AND b.id={} AND q.status='Q' GROUP BY b.id;".format(bestBranch)
+        cur.execute(query)
+        connection.commit()
+        rows = cur.fetchall()
+        for row in rows:
+            print("TEST {0} {1} {2} {3} {4} {5}".format(row[0],row[1],row[2],row[3],row[4],row[5]))
+            transactionResponse = {}
+            transactionResponse['id'] = row[0]
+            transactionResponse['name'] = row[1]
+            transactionResponse['district'] = row[2]
+            transactionResponse['addr'] = row[3]
+            transactionResponse['postal'] = row[4]
+            transactionResponse['contactNo'] = row[5]
+            transactionResponse['latt'] = row[6]
+            transactionResponse['longt'] = row[7]
+            transactionResponse['clinicId'] = row[8]
+            transactionResponse['queueLength'] = row[9]
+            branchList.append(transactionResponse)
 
 # Construct http response object
     responseObject = {}
